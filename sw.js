2ce7139a6100b4d52448fa38168b3edf2a1ba482
aquaphoenix1 <aqua_phoenix@mail.ru>
2019-04-28 19:33:21 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,28 @@
 const cacheName = 'cache-v1';
 const precacheResources = [
   'index.html',
-  'notesList.html'
+  'notesList.html',
+  'libs/bootbox.min.js',
+  'libs/bootstrap.min.js',
+  'libs/jquery-3.0.0.min.js',
+  'libs/jquery.cookie.js',
+  'libs/jquery.validate.min.js',
+  'styles/mainStyle.css',
+  'styles/notesListStyle.css',
+  'image/apple-icon-57x57.png',
+  'image/apple-icon-60x60.png',
+  'image/apple-icon-72x72.png',
+  'image/apple-icon-76x76.png',
+  'image/apple-icon-114x114.png',
+  'image/apple-icon-120x120.png',
+  'image/apple-icon-144x144.png',
+  'image/apple-icon-152x152.png',
+  'image/apple-icon-180x180.png',
+  'image/android-icon-192x192.png',
+  'image/favicon-32x32.png',
+  'image/favicon-96x96.png',
+  'image/favicon-16x16.png',
+  'image/ms-icon-144x144.png'
 ];
 
 self.addEventListener('install', event => {
