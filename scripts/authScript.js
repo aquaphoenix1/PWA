6a202751f2d93188daa2920bc9af39a059b552db
aquaphoenix1 <aqua_phoenix@mail.ru>
2019-04-28 19:48:59 +0400

    Update authScript.js

--- scripts/authScript.js
+++ scripts/authScript.js
@@ -60,7 +60,6 @@ function sendLoginData(login, password, success, error){
         error: (err, errText, errThrown) => {
             console.error('ajax error:' + errThrown);
             error();
-            success("123");
         }
     });
 }
