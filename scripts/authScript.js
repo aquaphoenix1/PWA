7460af9d25374666776ce2d98b06291d7b5489dc
aquaphoenix1 <aqua_phoenix@mail.ru>
2019-04-28 14:28:32 +0400

    Add files via upload

---
+++ scripts/authScript.js
@@ -0,0 +1,58 @@
+﻿function sendLogin(){
+    $("#authFormWrapper").validate({
+             rules:{
+                login:{
+                  required: true,
+                },
+                pswd:{
+                  required: true,
+                },
+             },
+             messages:{
+               login:{
+                 required: "Это поле обязательно для заполнения",
+             },
+               pswd:{
+               required: "Это поле обязательно для заполнения",
+               },
+             },
+             submitHandler: () => {
+                let login = $('#login').val();
+
+                console.log('send login: ' + login);
+
+                let password = $('#password').val();
+
+                 let success = function (login, response) {
+                     window.location.href = "/NoteList";
+                }
+
+                let error = function(){
+                    alert("Ошипка");
+                }
+
+                sendLoginData(login, password, success, error);
+             }
+          });
+}
+
+
+function sendLoginData(login, password, success, error){
+    $.ajax({
+        method: "GET",
+        url: "/login",
+        async: false,
+        data: {
+            login: login,
+            password: password
+        },
+        success:(response) => {
+            console.log('ajax success');
+            success(login, response);
+        },
+        error: (err, errText, errThrown) => {
+            console.error('ajax error:' + errThrown);
+            error();
+        }
+    });
+}
